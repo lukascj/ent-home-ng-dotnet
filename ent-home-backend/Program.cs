baf0d61ee3424f04759fabd36319eb6ef8535d86
Lukas J <lukascj04@gmail.com>
2024-08-22 18:04:54 +0200

    Återgenererat backend
    för att något publicerats som är alldeles för stort/något som skulle ignorerats vid commit
    
    dotnet new webapi --use-controllers -o ent-home-backend

--- ent-home-backend/Program.cs
+++ ent-home-backend/Program.cs
@@ -1,49 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-using ent_home_backend.Models;
-
-static EntDbContext GetContext(IServiceProvider serviceProvider) 
-{
-    IServiceScope scope = serviceProvider.CreateScope();
-    IServiceProvider services = scope.ServiceProvider;
-    return services.GetRequiredService<EntDbContext>();
-}
-static void ResetDb(EntDbContext context) 
-{
-    context.Database.ExecuteSqlRaw("DROP SCHEMA public CASCADE;");
-    context.Database.ExecuteSqlRaw("CREATE SCHEMA public;");
-}
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "Allow EntHome", policy  => 
-    {
-        policy.WithOrigins("http://localhost:4200").AllowAnyHeader(); 
-    });
-});
-
 // Add services to the container.
-var conn = builder.Configuration.GetConnectionString("DefaultConnection"); // Hämtar ConnectionString från appsettings.json
-builder.Services.AddDbContext<EntDbContext>(opt => {
-    opt.UseNpgsql(conn);
-}); // Registrerar EntDbContext så att appen kan använda den där den behövs.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-if(args.Contains("reset")) // "reset" är ett argument som kan skickas med när man kör: "dotnet run reset".
-{
-    EntDbContext dbContext = GetContext(app.Services);
-    ResetDb(dbContext);
-    return;
-}
-
-app.MapGet("/", () => "Hello World.");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -53,7 +17,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("Allow EntHome");
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
--- ent-home-backend/dev.sh
+++
@@ -1,5 +0,0 @@
-if [ $# -eq 0 ] # om mängden argument är 0
-  then
-    dotnet build # kompilerar, innebär automatisk "dotnet restore"
-    dotnet run dev --launch-profile https
-fi
