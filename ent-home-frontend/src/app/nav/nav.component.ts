1fae9782f12a2b4ed5bbcbc273478291bee87729
Lukas Johansson <lukascj04@gmail.com>
2024-09-25 19:39:24 +0200

    Lite nav-ändringar
    Gav upp på en överkomplicerad idé.

--- ent-home-frontend/src/app/nav/nav.component.ts
+++ ent-home-frontend/src/app/nav/nav.component.ts
@@ -16,23 +16,5 @@ export class NavComponent implements AfterViewInit {
   constructor(private route: ActivatedRoute) {}
 
   ngAfterViewInit() {
-    const navOptions = this.wrapElem.nativeElement.querySelectorAll('nav > a');
-    const navOptionsData = [];
-
-    console.log(this.route.url);
-
-
-    /* const resizeObserver = new ResizeObserver(entries => {
-      for (let entry of entries) {
-        console.log('Resized width:', entry.contentRect.width);
-        console.log('Resized offset-width:', entry.contentRect.offsetWidth);
-      }
-    }); */
-  
-    // resizeObserver.observe(this.wrapElem.nativeElement);
-    // console.log(this.wrapElem.nativeElement.getBoundingClientRect);
-    console.log(window.innerWidth);
-    console.log(this.wrapElem.nativeElement.offsetWidth);
-    console.log(this.wrapElem.nativeElement.getBoundingClientRect());
   }
 }  
